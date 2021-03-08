514638b09a2156b8c26066ec4da73886607fd23f
Anh <vuonga1103@yahoo.com>
2021-03-08 18:45:33 -0500

    completed uniqueBy

--- src/exercises/exercise_1_uniqueBy/uniqueBy.js
+++ src/exercises/exercise_1_uniqueBy/uniqueBy.js
@@ -1,3 +1,13 @@
 export default function uniqueBy(data, key) {
-  // * Write function here
+  const uniqueObjHash = {}; // Stores unique objects in a hash, where each key is the value of the original object's key. And each value is the most up-to-date object. Ex. {'Person 1' => {...}, ...}
+
+  data.forEach(addUniqueToHash);
+  return Object.values(uniqueObjHash);
+
+  function addUniqueToHash(obj) {
+    let type = obj[key];
+    uniqueObjHash[type] = uniqueObjHash.hasOwnProperty(type)
+      ? { ...uniqueObjHash[type], ...obj }
+      : obj;
+  }
 }
