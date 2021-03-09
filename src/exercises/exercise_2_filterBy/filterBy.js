ba9eb5fff2845e93caa4d2a6d63f5b84e4e1c490
Anh <vuonga1103@yahoo.com>
2021-03-08 19:25:30 -0500

    completed filterBy

--- src/exercises/exercise_2_filterBy/filterBy.js
+++ src/exercises/exercise_2_filterBy/filterBy.js
@@ -1,3 +1,17 @@
 export default function filterBy(data, searchTerm, keys) {
-  // * Write function here
+  return data.filter((obj) => {
+    let vals = [];
+    for (const [k, v] of Object.entries(obj)) {
+      if (keys.includes(k)) vals.push(v)
+    };
+
+    return checkSearchTermPresence(vals, searchTerm);
+  })
+}
+
+function checkSearchTermPresence(vals, searchTerm) {
+  return vals.some((val) => {
+    let re = new RegExp(searchTerm, "i");
+    return re.test(val)
+  });
 }
