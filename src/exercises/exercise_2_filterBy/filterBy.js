c5c01e3ef300b62091f76f2c61233dc56f2c8baa
Anh <vuonga1103@yahoo.com>
2021-03-08 19:29:19 -0500

    refactored filterBy

--- src/exercises/exercise_2_filterBy/filterBy.js
+++ src/exercises/exercise_2_filterBy/filterBy.js
@@ -1,17 +1,21 @@
 export default function filterBy(data, searchTerm, keys) {
   return data.filter((obj) => {
-    let vals = [];
-    for (const [k, v] of Object.entries(obj)) {
-      if (keys.includes(k)) vals.push(v)
-    };
-
+    let vals = getValsOfObjKeys(obj, keys);
     return checkSearchTermPresence(vals, searchTerm);
-  })
+  });
+}
+
+function getValsOfObjKeys(obj, keys) {
+  let vals = [];
+  for (const [k, v] of Object.entries(obj)) {
+    if (keys.includes(k)) vals.push(v);
+  };
+  return vals;
 }
 
 function checkSearchTermPresence(vals, searchTerm) {
   return vals.some((val) => {
     let re = new RegExp(searchTerm, "i");
-    return re.test(val)
+    return re.test(val);
   });
 }
